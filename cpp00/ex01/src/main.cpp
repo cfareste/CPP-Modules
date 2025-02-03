5c246f0a958289158616acb99af2c44ef3505c8e
cfareste <chrisfidare@yahoo.com>
2025-02-03 15:58:02 +0100

    feat: add command execution structure

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -2,33 +2,31 @@
 #include "PhoneBook/PhoneBook.hpp"
 #include <iostream>
 
+static std::string	read_command(void)
+{
+	std::string	input;
+
+	std::cout << "Enter a command: ";
+	std::cin >> input;
+	return input;
+}
+
+static void	execute_command(std::string &command)
+{
+	if (command == "ADD")
+		std::cout << "Adding contact" << std::endl;
+	else if (command == "SEARCH")
+		std::cout << "Searching contact" << std::endl;
+}
+
 int	main(void)
 {
-	Contact	c1 = Contact("Contact1", "c1", 637624279);
-	Contact	c2 = Contact("Contact2", "c2", 637624279);
-	Contact	c3 = Contact("Contact3", "c3", 637624279);
-	Contact	c4 = Contact("Contact4", "c4", 637624279);
-	Contact	c5 = Contact("Contact5", "c5", 637624279);
-	Contact	c6 = Contact("Contact6", "c6", 637624279);
-	Contact	c7 = Contact("Contact7", "c7", 637624279);
-	Contact	c8 = Contact("Contact8", "c8", 637624279);
-	Contact	c9 = Contact("Contact9", "c9", 637624279);
+	std::string	command = "";
 
-	PhoneBook	phone_book;
-
-	phone_book.add_contact(c1);
-	phone_book.add_contact(c2);
-	phone_book.add_contact(c3);
-	phone_book.add_contact(c4);
-	phone_book.add_contact(c5);
-	phone_book.add_contact(c6);
-	phone_book.add_contact(c7);
-	phone_book.add_contact(c8);
-
-	phone_book.list_contacts();
-	std::cout << std::endl;
-
-	phone_book.add_contact(c9);
-
-	phone_book.list_contacts();
+	while (command != "EXIT")
+	{
+		command = read_command();
+		execute_command(command);
+	}
+	return 0;
 }
