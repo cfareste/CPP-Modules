b623fb8b5a456e3f2c91d87a1833f47f3d87397c
cfareste <chrisfidare@yahoo.com>
2025-02-27 15:12:29 +0100

    feat: add destructors logs

--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -45,6 +45,8 @@ Account::Account(int initial_deposit)
 
 Account::~Account(void)
 {
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 }
 
 void	Account::makeDeposit(int deposit)
