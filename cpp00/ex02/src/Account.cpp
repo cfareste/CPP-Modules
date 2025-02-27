85913f21108870ab7a665456af0e04675184d769
cfareste <chrisfidare@yahoo.com>
2025-02-27 14:58:16 +0100

    feat: add displayTimestamp function and Account constructor

--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -1,4 +1,11 @@
 #include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 int	Account::getNbAccounts(void)
 {
@@ -26,7 +33,14 @@ void	Account::displayAccountsInfos(void)
 
 Account::Account(int initial_deposit)
 {
-	(void) initial_deposit;
+	this->_accountIndex = Account::_nbAccounts;
+	Account::_nbAccounts++;
+	this->_amount = initial_deposit;
+	Account::_nbDeposits = 0;
+	Account::_nbWithdrawals = 0;
+	Account::_totalAmount += initial_deposit;
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
 }
 
 Account::~Account(void)
@@ -55,4 +69,8 @@ void	Account::displayStatus(void) const
 
 void	Account::_displayTimestamp(void)
 {
+    std::time_t	t = std::time(nullptr);
+    std::tm*	now = std::localtime(&t);
+
+    std::cout << std::put_time(now, "[%Y%m%d_%H%M%S] ");
 }
