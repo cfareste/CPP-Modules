73438ca03ceea093022d3cb9784036943f536a63
cfareste <chrisfidare@yahoo.com>
2025-02-27 13:14:54 +0100

    feat: fix tests compilation

--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -1,1 +1,58 @@
 #include "Account.hpp"
+
+int	Account::getNbAccounts(void)
+{
+	return 0;
+}
+
+int	Account::getTotalAmount(void)
+{
+	return 0;
+}
+
+int	Account::getNbDeposits(void)
+{
+	return 0;
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return 0;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+}
+
+Account::Account(int initial_deposit)
+{
+	(void) initial_deposit;
+}
+
+Account::~Account(void)
+{
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	(void) deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	(void) withdrawal;
+	return false;
+}
+
+int	Account::checkAmount(void) const
+{
+	return 0;
+}
+
+void	Account::displayStatus(void) const
+{
+}
+
+void	Account::_displayTimestamp(void)
+{
+}
