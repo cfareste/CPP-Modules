9052672544566d0f7c1725e79a922c3a4a0777a2
cfareste <chrisfidare@yahoo.com>
2025-02-28 13:50:22 +0100

    feat: add main function to test zombie hordes (finish cpp00 ex01)

--- cpp01/ex01/src/main.cpp
+++ cpp01/ex01/src/main.cpp
@@ -1,6 +1,18 @@
 #include "Zombie/Zombie.hpp"
+#include <iostream>
 
 int	main()
 {
+	int	horde_size = 10;
+
+	Zombie	*horde = zombieHorde(horde_size, "NPC zombie");
+
+	for (int i = 0; i < horde_size; i++)
+	{
+		std::cout << i + 1 << ". ";
+		horde[i].announce();
+	}
+
+	delete[] horde;
 	return 0;
 }
