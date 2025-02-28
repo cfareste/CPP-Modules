2ae11f948623e2aa630b2c360e4536279e70f899
cfareste <chrisfidare@yahoo.com>
2025-02-28 14:26:02 +0100

    feat: add main function (finish cpp01 ex02)

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -1,4 +1,19 @@
+#include <string>
+#include <iostream>
+
 int	main()
 {
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &brain;
+	std::string	&stringREF = brain;
+
+	std::cout << "Brain memory address: " << &brain << std::endl;
+	std::cout << "Memory address held by pointer: " << stringPTR << std::endl;
+	std::cout << "Brain reference memory address: " << &stringREF << "\n" << std::endl;
+
+	std::cout << "Brain string value: " << brain << std::endl;
+	std::cout << "Pointer string value: " << *stringPTR << std::endl;
+	std::cout << "Reference string value: " << stringREF << std::endl;
+
 	return 0;
 }
