915901b87888b037f5c991c0a8705b364753ba26
cfareste <chrisfidare@yahoo.com>
2025-03-03 12:51:19 +0100

    feat: add Weapon class

---
+++ cpp01/ex03/src/Weapon/Weapon.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+class Weapon
+{
+	private:
+		std::string	type;
+
+	public:
+		Weapon();
+		Weapon(std::string &type);
+
+		const std::string	&getType();
+		void				setType(std::string &newType);
+
+		~Weapon();
+};
