81e98d1ea91625402a130d684726fdfbe328cebc
cfareste <chrisfidare@yahoo.com>
2025-03-03 16:30:37 +0100

    refactor: extract files opening to its own private method

--- cpp01/ex04/src/Replacer/Replacer.cpp
+++ cpp01/ex04/src/Replacer/Replacer.cpp
@@ -2,25 +2,31 @@
 #include <fstream>
 #include <iostream>
 
-int Replacer::replace(std::string path, std::string occurrence, std::string replacement)
+bool Replacer::open_files(std::string &path, std::ifstream &input_file, std::ofstream &output_file)
 {
-	std::ifstream	input_file;
-	std::ofstream	output_file;
-	std::string		line;
-
 	input_file.open(path, std::ios::in);
 	if (!input_file.is_open())
 	{
 		std::cout << "Error while opening '" << path << "' file" << std::endl;
-		return 1;
+		return false;
 	}
 	output_file.open(path + ".replace", std::ios::trunc);
 	if (!output_file.is_open())
 	{
 		std::cout << "Error while opening output file" << std::endl;
 		input_file.close();
-		return 1;
+		return false;
 	}
+	return true;
+}
+
+int Replacer::replace(std::string path, std::string occurrence, std::string replacement)
+{
+	std::ifstream	input_file;
+	std::ofstream	output_file;
+	std::string		line;
+
+	if (!Replacer::open_files(path, input_file, output_file)) return 1;
 
 	while (std::getline(input_file, line))
 	{
--- cpp01/ex04/src/Replacer/Replacer.hpp
+++ cpp01/ex04/src/Replacer/Replacer.hpp
@@ -3,6 +3,9 @@
 #include <string>
 
 class Replacer {
+	private:
+		static bool	open_files(std::string &path, std::ifstream &input_file, std::ofstream &output_file);
+
 	public:
 		static int	replace(std::string path, std::string occurrence, std::string replacement);
 };
