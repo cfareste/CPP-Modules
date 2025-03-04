74ce0775684312f2579c99dd04eb2ba90b269330
cfareste <chrisfidare@yahoo.com>
2025-03-04 12:18:26 +0100

    feat: add main functionality (finish cpp01)

--- cpp01/ex06/src/main.cpp
+++ cpp01/ex06/src/main.cpp
@@ -1,13 +1,16 @@
 #include "Harl/Harl.hpp"
+#include <iostream>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	Harl	harl = Harl();
+	Harl	harl;
 
-	harl.complain("UNKNOWN COMPLAIN");
-	harl.complain("DEBUG");
-	harl.complain("INFO");
-	harl.complain("WARNING");
-	harl.complain("ERROR");
-	harl.complain("UNKNOWN COMPLAIN");
+	if (argc != 2)
+	{
+		std::cout << "Wrong usage: harlFilter [complain]" << std::endl;
+		return 1;
+	}
+
+	harl.complain(argv[1]);
+	return 0;
 }
