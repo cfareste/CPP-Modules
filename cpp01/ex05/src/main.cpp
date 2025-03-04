4a8916fbd21d26b2308b38a29157ea976f75c9d0
cfareste <chrisfidare@yahoo.com>
2025-03-04 11:32:44 +0100

    feat: add main functionality (finish cpp01 ex05)

--- cpp01/ex05/src/main.cpp
+++ cpp01/ex05/src/main.cpp
@@ -1,4 +1,13 @@
+#include "Harl/Harl.hpp"
+
 int	main(void)
 {
-	return 0;
+	Harl	harl = Harl();
+
+	harl.complain("UNKNOWN COMPLAIN");
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("UNKNOWN COMPLAIN");
 }
