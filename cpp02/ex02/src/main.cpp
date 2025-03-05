6c8025ab8c9a787aca524904eb9b0663c26dece5
cfareste <chrisfidare@yahoo.com>
2025-03-05 17:17:32 +0100

    feat: add main functionality (finish cpp02 ex02)

--- cpp02/ex02/src/main.cpp
+++ cpp02/ex02/src/main.cpp
@@ -3,5 +3,33 @@
 
 int	main(void)
 {
+	Fixed		a;
+	const Fixed	b = Fixed(Fixed(5.05f) * Fixed(2));
+	Fixed		c = b;
+	Fixed		d = Fixed(23.32f);
+
+	std::cout << ((a < b) ? "true" : "false") << std::endl;
+	std::cout << ((a > b) ? "true" : "false") << std::endl;
+	std::cout << ((b <= c) ? "true" : "false") << std::endl;
+	std::cout << ((b >= a) ? "true" : "false") << std::endl;
+	std::cout << ((b == c) ? "true" : "false") << std::endl;
+	std::cout << ((b != c) ? "true" : "false") << std::endl;
+
+	std::cout << c + d << std::endl;
+	std::cout << c - d << std::endl;
+	std::cout << c * d << std::endl;
+	std::cout << c / d << std::endl;
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << Fixed::min(a, b) << std::endl;
+
 	return 0;
 }
