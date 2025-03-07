60cf28d6549abbb64c3819be98f6f5d0e6165ecf
cfareste <chrisfidare@yahoo.com>
2025-03-07 17:39:37 +0100

    feat: add main function

--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -1,6 +1,32 @@
-
+#include "ClapTrap/ClapTrap.hpp"
+#include <iostream>
 
 int	main(void)
 {
+	ClapTrap	trinity = ClapTrap("Trinity");
+	ClapTrap	xanax = ClapTrap("Xanax");
+	ClapTrap	antares = ClapTrap("Antares");
+
+	std::cout << trinity << std::endl;
+	std::cout << xanax << std::endl;
+	std::cout << antares << std::endl;
+
+	trinity.attack("Xanax");
+	xanax.attack("Antares");
+	xanax.takeDamage(4);
+	antares.takeDamage(12);
+
+	std::cout << trinity << std::endl;
+	std::cout << xanax << std::endl;
+	std::cout << antares << std::endl;
+
+	xanax.beRepaired(3);
+	antares.beRepaired(4);
+	antares.attack("Reggin");
+
+	std::cout << trinity << std::endl;
+	std::cout << xanax << std::endl;
+	std::cout << antares << std::endl;
+
 	return 0;
 }
