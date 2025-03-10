5e411c6b6c9f49b84d99cdee9f49f8fa428e1907
cfareste <chrisfidare@yahoo.com>
2025-03-10 13:33:04 +0100

    chore: add cpp03 ex01 structure

---
+++ cpp03/ex01/src/ClapTrap/ClapTrap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+
+class ClapTrap {
+	private:
+		std::string	name;
+		int			hit_points;
+		int			energy_points;
+		int			attack_damage;
+
+		ClapTrap();
+
+	public:
+		ClapTrap(const std::string &name);
+		ClapTrap(const ClapTrap &ct);
+
+		std::string	&getName();
+		int			getHitPoints();
+		int			getEnergyPoints();
+
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+		ClapTrap	&operator=(const ClapTrap &ct);
+
+		~ClapTrap();
+};
+
+std::ostream	&operator<<(std::ostream &stream, ClapTrap &clap_trap);
