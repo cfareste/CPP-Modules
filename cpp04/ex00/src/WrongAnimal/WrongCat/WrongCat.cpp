779948bbb5cd95eaac78ed93507fd1b9ac8e0e5f
cfareste <chrisfidare@yahoo.com>
2025-03-13 10:32:39 +0100

    feat: add WrongCat class

---
+++ cpp04/ex00/src/WrongAnimal/WrongCat/WrongCat.cpp
@@ -0,0 +1,33 @@
+#include "WrongCat.hpp"
+#include <iostream>
+
+WrongCat::WrongCat()
+{
+	std::cout << "WrongCat constructor called" << std::endl;
+	this->type = "WrongCat";
+}
+
+WrongCat::WrongCat(const WrongCat &wrong_cat)
+{
+	std::cout << "WrongCat copy constructor called" << std::endl;
+	*this = wrong_cat;
+}
+
+void	WrongCat::makeSound()
+{
+	std::cout << "!WWOOOEEEEM" << std::endl;
+}
+
+WrongCat	&WrongCat::operator=(const WrongCat &wrong_cat)
+{
+	std::cout << "WrongCat copy assignment constructor called" << std::endl;
+	if (this == &wrong_cat) return *this;
+
+	this->type = wrong_cat.type;
+	return *this;
+}
+
+WrongCat::~WrongCat()
+{
+	std::cout << "WrongCat destructor called" << std::endl;
+}
