02b244af4b6a587b640186459a76cb6ff6ebe2b9
cfareste <chrisfidare@yahoo.com>
2025-03-14 15:16:59 +0100

    feat: add Ice class

---
+++ cpp04/ex03/src/Materia/Ice/Ice.cpp
@@ -0,0 +1,34 @@
+#include "Ice.hpp"
+#include <iostream>
+
+Ice::Ice()
+{
+	std::cout << "Ice default constructor called" << std::endl;
+}
+
+Ice::Ice(const Ice &ice)
+{
+	std::cout << "Ice copy constructor called" << std::endl;
+	*this = ice;
+}
+
+AMateria	*Ice::clone() const
+{
+	return new Ice(*this);
+}
+
+void	Ice::use(/*ICharacter &target*/)
+{
+	std::cout << "* shoots an ice bolt at " /*<< target.getName()*/ << " *" << std::endl;
+}
+
+Ice	&Ice::operator=(const Ice &ice)
+{
+	std::cout << "Ice copy assignment constructor called" << std::endl;
+	return *this;
+}
+
+Ice::~Ice()
+{
+	std::cout << "Ice destructor called" << std::endl;
+}
---
+++ cpp04/ex03/src/Materia/Ice/Ice.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Materia/AMateria.hpp"
+
+class Ice : public AMateria {
+	public:
+		Ice();
+		Ice(const Ice &ice);
+
+		AMateria	*clone() const;
+		void		use(/*ICharacter &target*/);
+
+		Ice	&operator=(const Ice &ice);
+
+		~Ice();
+};
