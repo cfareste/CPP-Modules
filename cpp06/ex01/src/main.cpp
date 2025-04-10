9191dbe1825d30ae58b255145b7b0349dc7c4db6
cfareste <chrisfidare@yahoo.com>
2025-04-10 12:05:13 +0200

    feat: add main tests (finish cpp06 ex01)

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -1,4 +1,27 @@
+#include "Serializer/Serializer.hpp"
+#include <iostream>
+
 int	main()
 {
+	Data		data = Data();
+	Data		*data_ptr;
+	uintptr_t	serialized_data;
+
+	std::cout << "Data value: " << data.getData() << std::endl;
+	std::cout << "Data address: " << &data << std::endl;
+
+	serialized_data = Serializer::serialize(&data);
+
+	std::cout << "Serialized data: " << serialized_data << std::endl;
+
+	data_ptr = Serializer::deserialize(serialized_data);
+
+	std::cout << "Deserialized data: " << data_ptr->getData() << std::endl;
+	std::cout << "Deserialized data addres: " << data_ptr << std::endl;
+
+	serialized_data = Serializer::serialize(data_ptr);
+
+	std::cout << "New serialized data (deserialized-data serialized): " << serialized_data << std::endl;
+
 	return 0;
 }
