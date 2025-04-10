5fa5a47e401f7bca514daa369fd97325af41a98b
cfareste <chrisfidare@yahoo.com>
2025-04-10 12:30:41 +0200

    feat: add A, B and C classes

---
+++ cpp06/ex02/src/Base/B/B.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Base/Base.hpp"
+
+class B : public Base
+{
+	public:
+		B();
+		~B();
+};
---
+++ cpp06/ex02/src/Base/C/C.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Base/Base.hpp"
+
+class C : public Base
+{
+	public:
+		C();
+		~C();
+};
