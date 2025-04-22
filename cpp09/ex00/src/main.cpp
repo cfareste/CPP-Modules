657248a6a881a3ad5321fe3cd5198f2c9f0b7ee6
cfareste <chrisfidare@yahoo.com>
2025-04-22 20:15:12 +0200

    feat: add main tests (finish cpp09 ex01)

--- cpp09/ex00/src/main.cpp
+++ cpp09/ex00/src/main.cpp
@@ -1,6 +1,25 @@
+#include "BitcoinExchange/BitcoinExchange.hpp"
+#include <iostream>
+
+#define DATABASE_PATH "data.csv"
+
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
+	if (argc != 2)
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try
+	{
+		BitcoinExchange	btc = BitcoinExchange(DATABASE_PATH);
+
+		btc.calculatePricesFromFile(argv[1]);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
 	return 0;
 }
