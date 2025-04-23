21f9eb9685faa303eb1b2df1929a9dbc9ebabc5a
cfareste <chrisfidare@yahoo.com>
2025-04-23 22:14:48 +0200

    feat: add main tests (finish cpp09 ex01)

--- cpp09/ex01/src/main.cpp
+++ cpp09/ex01/src/main.cpp
@@ -1,6 +1,23 @@
+#include "RPN/RPN.hpp"
+#include <iostream>
+
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
+	if (argc != 2)
+	{
+		std::cerr << "Error" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try
+	{
+		RPN	rpn = RPN();
+
+		rpn.calculateRPN(argv[1]);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
 	return 0;
 }
